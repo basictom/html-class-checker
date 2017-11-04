1e4343dbaa68c5e64d3e0372cfb1fc9cac1fb7f0
Tom <basictom@githu.com>
2017-11-04 13:33:09 -0500

    almost there

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $('#checkIt').click(function() {
     var userInput = $('#inputHTML').val();
+    let fail = $('#fail').text();
+    let pass = $('#match').text();
     userInput = $.trim(userInput);
     let styleTop = userInput.indexOf("<style>");
     let styleBott = userInput.indexOf("</style>");
@@ -36,111 +38,33 @@ $(document).ready(function() {
         styles = userInput.substr(styleTop, styleTag);
       }
 
-      // console.log(styles);
+      // let classNames = styles.substr(".", styles.indexOf("{"));
+      let classes = styles.match(/\.([a-z0-9\-_])*/gi).map(function(str){
+        if(str.charAt(0) == "."){
+          str = str.slice(1);
+        }
+        // console.log(str);
+        return str;
+      });
 
-      console.log($(this).css("class"));
+      let matched;
+      let notMatched;
 
-      // console.log(sortArray);
-      // console.log(styles);
+      $.each(sortArray, function(index, htmlValue){
+        // console.log(value);
+        $.each(classes, function(i, headValue){
+          if(htmlValue == headValue){
+            console.log("matched");
+            matched = headValue;
+          }else{
+            console.log("in style tag but not HTML");
+            notMatched = htmlValue;
+          }
+        })
+      });
 
-      // $(sortArray).each(function(index, value){
-      //   console.log(index);
-      //   console.log(value);
-      //   console.log(styles.indexOf(value));
-      //   if(styles.indexOf(value)){
-      //     console.log("matched styles");
-      //   }else{
-      //     console.log("unmatched styles");
-      //   }
-      // });
+      fail = "Testing string";
 
     });
   })
 });
-
-
-// $( document ).ready(function() {
-//
-//  $( "#checkIt" ).click(function() {
-//  var myArray;
-//  var unique;
-//  var sourceStyles;
-//
-//  $("#results").css("display","none");
-//
-//  clearDivs();
-//
-//   myArray = [];
-//
-//   // grab the source code
-//   var txtHold = $("#inputHTML").val();
-//   txtHold = $.trim(txtHold);
-//   // get rid of all spaces (multi classes) first
-//   $(txtHold).find('[class]').each(function(index){
-//       var tmp = $(this).attr('class');
-// 	  if(tmp.indexOf(" ")!==-1){
-// 		tmp = tmp.replace(/ /g, ',');
-// 		tmp = tmp.split(',');
-// 		$.each(tmp,function(i,l){
-// 		 myArray.push(l);
-// 		});
-// 	  }else{
-// 		  myArray.push($(this).attr('class'));
-// 	  }
-//   });
-//
-//   // check to see that classes were found
-//   if(myArray == ""){
-// 	alert("Your input doesn't have any classes.");
-// 	clearDivs();
-// 	return;
-//   }
-//
-//   // get rid of the dupes in the array
-//   unique=myArray.filter(function(itm,i,a){
-//     if(itm){ return i==a.indexOf(itm); }
-//   });
-//
-//
-//   // sort it
-//   unique.sort();
-//
-//   // grab the <style> data from the input source code
-//   if(txtHold.indexOf('@media')==-1){
-// 	alert("Your input has no media query (mobile styles).");
-// 	clearDivs();
-//   }else{
-// 	sourceStyles = txtHold.substr(txtHold.indexOf('@media'),(txtHold.indexOf('</style>',txtHold.indexOf('@media'))-txtHold.indexOf('@media')));
-//   }
-//
-//   // loop through the array
-//   $( unique ).each(function(index,value){
-//
-// 	 // see if each class is present in the <style>
-// 	 if(value != undefined){
-//
-// 		 if(sourceStyles.indexOf(value)!=-1){
-//
-// 		  if(sourceStyles.indexOf(value+"]")!=-1 | sourceStyles.indexOf(value+" ]")!=-1 | sourceStyles.indexOf(value+"{")!=-1 | sourceStyles.indexOf(value+" {")!=-1){
-//         console.log(sourceStyles.indexOf(value));
-// 		   $( "#match" ).append("<div>" + value + "</div>");
-// 		  }else{
-// 		   $( "#fail" ).append("<div>" + value + "</div>");
-// 		  }
-// 		 }else{
-// 		  $( "#fail" ).append("<div>" + value + "</div>");
-// 		 }
-// 	 }
-// 	 $("#results").css("display","block");
-//
-//   });
-//
-//   function clearDivs(){
-//    // clear the results divs
-//    $( "#match" ).html("");
-//    $( "#fail" ).html("");
-//   }
-//
-//   });
-//
-// });
